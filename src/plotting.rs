82f028786b558473d60b1a3b1973a63b63a80ee8
Odin <odin.sev@gmail.com>
2023-10-29 09:37:55 +0100

    Will probably need bidirectional channels for communication between the simulator and GUI

--- src/plotting.rs
+++ src/plotting.rs
@@ -3,6 +3,7 @@ use eframe::{egui, epaint::Color32};
 use egui_plot::{Legend, Line, MarkerShape, Plot, PlotPoints, Points};
 use nalgebra::DVector;
 use std::collections::VecDeque;
+use tokio::sync::mpsc::{Sender, Receiver, channel};
 
 pub fn plot_states(
     fig_name: &str,
@@ -65,50 +66,70 @@ pub fn plot_states(
     Ok(())
 }
 
-// pub struct TrackerVizApp;
+pub struct BiDirectionalChannel<T> {
+    rx: Receiver<T>,
+    tx: Sender<T>,
+}
 
-// impl TrackerVizApp {
-//     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-//         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-//         // Restore app state using cc.storage (requires the "persistence" feature).
-//         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-//         // for e.g. egui::PaintCallback.
-//         MyEguiApp {
-//             roll_data: VecDeque::new(),
-//             pitch_data: VecDeque::new(),
-//             yaw_data: VecDeque::new(),
-//             buffer_size: BufferSize::Limited(100),
-//             rx,
-//         }
-//     }
-// }
+pub fn bidirectional_channel<T>(buffer: usize) -> (BiDirectionalChannel<T>, BiDirectionalChannel<T>) {
+    let (tx1, rx1) = channel(buffer);
+    let (tx2, rx2) = channel(buffer);
+    (BiDirectionalChannel {rx: rx1, tx: tx2}, BiDirectionalChannel {rx: rx2, tx: tx1})
+}
 
-// impl eframe::App for MyEguiApp {
-//     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-//         egui::CentralPanel::default().show(ctx, |ui| {
-//             ui.heading("Hello World!");
+impl<T> BiDirectionalChannel<T> {
+    pub async fn send(&self, value: T) -> Result<(), tokio::sync::mpsc::error::SendError<T>> {
+        self.tx.send(value).await
+    }
 
-//             let my_plot = Plot::new("Euler angles").legend(Legend::default());
+    pub async fn recv(&mut self) -> Option<T> {
+        self.rx.recv().await
+    }
+}
 
-//             // let's create a dummy line in the plot
-//             let _inner = my_plot.show(ui, |plot_ui| {
-//                 plot_ui.line(
-//                     Line::new(self.roll_data.iter().copied().collect::<PlotPoints>())
-//                         .name("Roll")
-//                         .color(Color32::GREEN),
-//                 );
-//                 plot_ui.line(
-//                     Line::new(self.pitch_data.iter().copied().collect::<PlotPoints>())
-//                         .name("Pitch")
-//                         .color(Color32::RED),
-//                 );
-//                 plot_ui.line(
-//                     Line::new(self.yaw_data.iter().copied().collect::<PlotPoints>())
-//                         .name("Yaw")
-//                         .color(Color32::BLUE),
-//                 );
-//             });
-//         });
-//         ctx.request_repaint();
-//     }
-// }
+
+// pub fn init_viz() -> 
+
+
+pub struct TrackerVizApp;
+
+impl TrackerVizApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        TrackerVizApp {
+        }
+    }
+}
+
+impl eframe::App for TrackerVizApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+
+            let my_plot = Plot::new("Euler angles").legend(Legend::default());
+
+            // let's create a dummy line in the plot
+            let _inner = my_plot.show(ui, |plot_ui| {
+                plot_ui.line(
+                    Line::new(self.roll_data.iter().copied().collect::<PlotPoints>())
+                        .name("Roll")
+                        .color(Color32::GREEN),
+                );
+                plot_ui.line(
+                    Line::new(self.pitch_data.iter().copied().collect::<PlotPoints>())
+                        .name("Pitch")
+                        .color(Color32::RED),
+                );
+                plot_ui.line(
+                    Line::new(self.yaw_data.iter().copied().collect::<PlotPoints>())
+                        .name("Yaw")
+                        .color(Color32::BLUE),
+                );
+            });
+        });
+        ctx.request_repaint();
+    }
+}
